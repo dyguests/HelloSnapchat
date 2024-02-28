57f93ccc124b0651cded03d22e4d57a481f45b40
fanhl <dyguests@qq.com>
2024-02-28 10:51:19 +0800

    要将自定义按钮添加到您的应用程序以用于登录流程

--- app/src/main/java/com/fanhl/hellosnapchat/MainActivity.kt
+++ app/src/main/java/com/fanhl/hellosnapchat/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.fanhl.hellosnapchat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.snap.loginkit.LoginResultCallback
+import com.snap.loginkit.SnapLoginProvider
+import com.snap.loginkit.exceptions.LoginException
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val snapLogin = SnapLoginProvider.get(this)
+        findViewById<Button>(R.id.btn_snapchat).setOnClickListener {
+            snapLogin.startTokenGrant(object : LoginResultCallback {
+                override fun onStart() {
+                    Log.d(TAG, "fanhl MainActivity onStart")
+                }
+
+                override fun onSuccess(accessToken: String) {
+                    Log.d(TAG, "fanhl MainActivity onSuccess accessToken:$accessToken")
+                }
+
+                override fun onFailure(exception: LoginException) {
+                    Log.e(TAG, "fanhl MainActivity onFailure", exception)
+                }
+            })
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
